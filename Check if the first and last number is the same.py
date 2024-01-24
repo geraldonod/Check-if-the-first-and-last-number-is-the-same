971e36a3d7d0de7932d509da420ed8a5a6e3170e
Gerald <geraldonod@gmail.com>
2024-01-24 23:18:40 +0800

    added given number lists

--- Check if the first and last number is the same.py
+++ Check if the first and last number is the same.py
@@ -1,5 +1,10 @@
-list = [10, 20, 30, 40, 10]
-if list[0] == list[-1]:
-    print('Result is True')
-else:
-    print('Result is False')
+#given list 1
+numbers_x = [10, 20, 30, 40, 10]
+print(numbers_x)
+#if else statement that checks if list 1 has the same first and last number
+
+#given list 2
+numbers_y = [75, 65, 35, 75, 30]
+print(numbers_y)
+
+#if else statement that checks if list 2 has the same first and last number
