c713e44b8f8486be4d41c7da34d6161342b5f303
Gerald <geraldonod@gmail.com>
2024-01-24 23:42:17 +0800

    added if else statements

--- Check if the first and last number is the same.py
+++ Check if the first and last number is the same.py
@@ -2,9 +2,17 @@
 numbers_x = [10, 20, 30, 40, 10]
 print(numbers_x)
 #if else statement that checks if list 1 has the same first and last number
+if numbers_x[0] == numbers_x[-1]:
+    print('Result is True for list 1')
+else:
+    print('Result is False for list 1')
 
 #given list 2
 numbers_y = [75, 65, 35, 75, 30]
 print(numbers_y)
 
 #if else statement that checks if list 2 has the same first and last number
+if numbers_y[0] == numbers_y[-1]:
+    print('Result is True for list 2')
+else:
+    print('Result is False for list 2')
